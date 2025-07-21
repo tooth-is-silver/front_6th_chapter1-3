68326113a634cf399bc5b87d7e2f15b3cd915cfa
hmyo2853 <hmyo2853@gmail.com>
2025-07-21 23:24:35 +0900

    Refactor : shallowEquals 함수 리팩토링

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,41 +1,52 @@
-/**
- * 두 값이 얕은 비교를 수행합니다.
- * shallow : 얕은
- * equal : 동일하다
- * @param a 비교할 첫 번째 값
- * @param b 비교할 두 번째 값
- * @returns 두 값이 같은지 얕은 비교를 수행한 결과
- */
-export const shallowEquals = (a: unknown, b: unknown) => {
-  // 1. 두 값이 정확히 같나요?
-  if (a === b) return true;
-
-  // 두 값이 유효한 값인가요?
-  if (a === null || b === null || a === undefined || b === undefined) return false;
-
-  // 두 값이 배열인가요?
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) return false;
-
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) return false;
-    }
+export function shallowEquals(a: unknown, b: unknown): boolean {
+  // 두 값이 완전히 동일한가요?
+  if (Object.is(a, b)) {
+    return true;
   }
 
-  // 2. 둘 다 객체인가요?
-  if (typeof a !== "object" || typeof b !== "object") return false;
+  // null이거나 undefined가 있나요?
+  if (a == null || b == null) {
+    return a === b;
+  }
+
+  // 동일한 함수인가요?
+  if (typeof a === "function" && typeof b === "function") {
+    return a.toString() === b.toString();
+  }
+
+  // object가 아닌가요?
+  if (typeof a !== "object" || typeof b !== "object") {
+    return false;
+  }
+
+  // 생성자가 같은가요?
+  if (a.constructor !== b.constructor) {
+    return false;
+  }
+
+  // 동일한 Date 값인가요?
+  if (a instanceof Date && b instanceof Date) {
+    return a.getTime() === b.getTime();
+  }
+
+  // 동일한 RegExp 값인가요?
+  if (a instanceof RegExp && b instanceof RegExp) {
+    return a.source === b.source && a.flags === b.flags;
+  }
 
   // 둘 다 객체네요.
-  // 3. 그럼 객체의 키 개수가 같은가요?
+  // 두 객체의 키 개수가 같은가요?
   const aKeys = Object.keys(a);
   const bKeys = Object.keys(b);
 
   if (aKeys.length !== bKeys.length) return false;
 
-  // 4. 모든 키가 같은가요?
+  // 모든 키가 같은가요?
   for (const key of aKeys) {
-    if (a[key as keyof typeof a] !== b[key as keyof typeof b]) return false;
+    if (a[key as keyof typeof a] !== b[key as keyof typeof b]) {
+      return false;
+    }
   }
 
   return true;
-};
+}
