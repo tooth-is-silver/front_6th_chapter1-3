e0b1dd7a0957f1506d69533b2d8e255ed66efbee
hmyo2853 <hmyo2853@gmail.com>
2025-07-24 23:37:37 +0900

    Refactor : useShallowState 의존성 배열이슈 개선

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,15 +1,16 @@
-import { useCallback, useState, type Dispatch, type SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import { shallowEquals } from "../equals";
+import { useAutoCallback } from "./useAutoCallback";
 
 export const useShallowState = <T>(initialValue: T): [T, Dispatch<SetStateAction<T>>] => {
   const [value, setValue] = useState(initialValue);
 
-  const setShallowValue = useCallback((newValue: SetStateAction<T>) => {
+  const setShallowValue = useAutoCallback((newValue: SetStateAction<T>) => {
     if (shallowEquals(value, newValue)) {
       return;
     }
     setValue(newValue);
-  }, []);
+  });
 
   return [value, setShallowValue];
 };
