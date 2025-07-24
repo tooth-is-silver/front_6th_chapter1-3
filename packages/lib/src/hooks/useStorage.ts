683f7d9525cbd260fe865e519369c098b82e542a
hmyo2853 <hmyo2853@gmail.com>
2025-07-24 13:30:45 +0900

    Feature : useStorage, useRouter 구현

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -4,6 +4,7 @@ import type { createStorage } from "../createStorage";
 type Storage<T> = ReturnType<typeof createStorage<T>>;
 
 export const useStorage = <T>(storage: Storage<T>) => {
-  // useSyncExternalStore를 사용해서 storage의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  return storage.get();
+  const { subscribe, get } = storage;
+
+  return useSyncExternalStore(subscribe, get);
 };
