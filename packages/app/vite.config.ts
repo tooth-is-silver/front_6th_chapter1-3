612c4f0f02a4f9f8a156a2cba4618adeadeed0b0
hmyo2853 <hmyo2853@gmail.com>
2025-07-25 01:54:03 +0900

    Feature : dist 파일 제외하고 모두 제거

--- packages/app/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react-oxc";
-import { createViteConfig } from "../../createViteConfig";
-
-const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-3/" : "";
-
-export default createViteConfig({
-  base,
-  plugins: [react()],
-});
--- packages/lib/src/hooks/useCallback.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-function-type */
-import type { DependencyList } from "react";
-import { useMemo } from "./useMemo";
-import { shallowEquals } from "../equals";
-
-export function useCallback<T extends Function>(factory: T, _deps: DependencyList, _equals = shallowEquals) {
-  return useMemo<T>(() => factory, _deps, _equals);
-}
